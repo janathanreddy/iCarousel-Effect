0ebe255e04bd0cfe7625bfd76de8bfcf81f5bf4f
janarthan s <janathanreddy@gmail.com>
2020-12-23 23:23:37 +0530

    first commit

---
+++ iCarouselEffect/iCarouseelCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  iCarouseelCollectionViewCell.swift
+//  iCarouselEffect
+//
+//  Created by Janarthan Subburaj on 23/12/20.
+//
+
+import UIKit
+
+class iCarouseelCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
